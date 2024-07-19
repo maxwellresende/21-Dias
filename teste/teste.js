fbd7799ccf8dd12df303f121a8c70affbb3cbe96
Maxwell Resende <maxwellresende60@gmail.com>
2024-07-19 20:36:48 -0300

    rodando teste-3

---
+++ teste/teste.js
@@ -0,0 +1,22 @@
+
+
+let nome = ""
+let idade = 0
+let altura = 0
+let peso = 0
+//
+
+
+nome = prompt("Digite seu nome:")
+idade = parseInt(prompt("Digite sua idade:"))
+altura = parseFloat(prompt("Digite sua altura:"))
+pese = parseInt(prompt("Digite seu peso:"))
+
+//
+
+let anoNasc = 0
+anoNasc = 2023 - idade
+//
+
+console.log("Olá" + nome +", voce tem " + idaded + " anos, nasceu em " + anoNasc +
+    ", tem " + altura + "de altura, pesa " + peso + "kg e seu IMC é " + imc + "kg/m²") 
