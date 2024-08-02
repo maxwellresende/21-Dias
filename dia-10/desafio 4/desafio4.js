4b301114d68d08ad65bc68f9ed325efca0518e79
Maxwell Resende <maxwellresende60@gmail.com>
2024-08-02 00:03:17 -0300

    treinamento do dia 10

---
+++ dia-10/desafio 4/desafio4.js
@@ -0,0 +1,15 @@
+//MAXWELL RESENDE
+
+let numeroEscolhido = Number(prompt("Insira aqui o numero Desejado!"))
+let numerosdeFibonacci = []
+
+numerosdeFibonacci[0] = numeroEscolhido - 1
+numerosdeFibonacci[1] = numeroEscolhido
+
+for (let i = 2; i < 10; i++) {
+    numerosdeFibonacci[i] = numerosdeFibonacci[i - 1] + numerosdeFibonacci[i - 2]
+}
+
+console.log("numeros de fibonacci: " + numerosdeFibonacci)
+
+    
