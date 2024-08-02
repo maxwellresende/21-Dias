d4e8ee1a085855633567421e28dae11632b99e6a
Maxwell Resende <maxwellresende60@gmail.com>
2024-08-02 01:15:40 -0300

    treinamento do dia 14

---
+++ dia-14/desafio14.js
@@ -0,0 +1,17 @@
+//MAXWELL RESENDE
+//CRIANDO CLASSE
+
+class Carro {
+    Modelo
+    Marca
+    Cor 
+    constructor(modelo, marca, cor) {
+        this.Modelo = modelo
+        this.Marca = marca
+        this.Cor = cor
+    }
+}
+
+//CRIANDO OBJETO
+
+let carro = new Carro("Fiesta", "Ford", "Prata")
